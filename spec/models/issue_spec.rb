fad6100408c5c4b1c9546e66a8f51f181d55bdd4
Leonardo Felix Moura <leonardo.moura@mcfox.com.br>
2011-11-26 11:53:19 -0200

    criando teste de email

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Issue do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Automação por e-mail" do
+    it "should create a new issue when receiving a valid e-mail"
+    it "should add a reply if there is a issue created"
+    it "should attach a file when receiveing a email with attachment"
+     end
+  end
 end
