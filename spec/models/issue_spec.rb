df4be1900a8ac9f8dd7d458a7cf9e3424a9ddcac
Leonardo Felix Moura <leonardo.moura@mcfox.com.br>
2011-11-26 12:32:01 -0200

    criando teste de email

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -2,9 +2,14 @@ require 'spec_helper'
 
 describe Issue do
   describe "Automação por e-mail" do
-    it "should create a new issue when receiving a valid e-mail"
+    it "should create a new issue when receiving a valid e-mail" do
+      msg= Mail::Message.new(File.read("#{Rails.root}/spec/files/email_simples.eml"))
+      msg.subject.should == "Erro no tela de apontamento"
+      msg.body.should match "problema na tela de apontamento"
+      msg.text_part.decoded.should match "tela de apontamento"
+    end
     it "should add a reply if there is a issue created"
     it "should attach a file when receiveing a email with attachment"
-     end
   end
 end
+
